3108e9244ac7b1888933e7035f92ac7099677d71
Nejcdm <ninzizaemmanuel22@gmail.com>
2026-09-09 07:42:39 +0100

    Fix output stream syntax for std::endl

--- chapitre-01/exo1-le_programme_minimal/c1-exo1_main.cpp
+++ chapitre-01/exo1-le_programme_minimal/c1-exo1_main.cpp
@@ -5,7 +5,7 @@ char nom[30];
   char ville[30];
 std::cin>>nom ;
 std::cin>>ville ;
-std::cout<<nom<<dts::endl;
-std::cout<<ville<<dtd::endl;
+std::cout<<nom<<std::endl;
+std::cout<<ville<<std::endl;
 return 0 ;
 }
