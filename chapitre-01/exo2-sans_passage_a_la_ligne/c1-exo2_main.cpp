23c93123b69de76154a849945fe000002be83ba2
Nejcdm <ninzizaemmanuel22@gmail.com>
2026-09-09 08:27:42 +0100

    Update c1-exo2_main.cpp

--- chapitre-01/exo2-sans_passage_a_la_ligne/c1-exo2_main.cpp
+++ chapitre-01/exo2-sans_passage_a_la_ligne/c1-exo2_main.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+int main()
+{
+char nom[30];
+  char ville[30];
+std::cout<<"entrer votre nom" ;
+std::cin.getline(nom,30);
+  std::cout<<"entrer votre ville" ;
+std::cin.getline(ville,30);
+std::cout<<nom ;
+std::cout<<ville ;
+return 0 ;
+}
