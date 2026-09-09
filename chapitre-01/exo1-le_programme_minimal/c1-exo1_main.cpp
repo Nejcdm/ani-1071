ebd2d9056879c6429788b2a746947d8d1d73b60a
Nejcdm <ninzizaemmanuel22@gmail.com>
2026-09-09 07:42:00 +0100

    Update output to use std::endl for line breaks
    Fix output formatting by replacing newline characters with std::endl.

--- chapitre-01/exo1-le_programme_minimal/c1-exo1_main.cpp
+++ chapitre-01/exo1-le_programme_minimal/c1-exo1_main.cpp
@@ -5,7 +5,7 @@ char nom[30];
   char ville[30];
 std::cin>>nom ;
 std::cin>>ville ;
-std::cout<<nom<<"\n";
-std::cout<<ville<<"\n";
+std::cout<<nom<<dts::endl;
+std::cout<<ville<<dtd::endl;
 return 0 ;
 }
