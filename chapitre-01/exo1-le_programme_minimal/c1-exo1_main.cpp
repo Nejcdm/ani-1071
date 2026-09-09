b0c4ff5468969a488119d88329232c712fa93fa6
Nejcdm <ninzizaemmanuel22@gmail.com>
2026-09-09 07:23:51 +0100

    Update c1-exo1_main.cpp

--- chapitre-01/exo1-le_programme_minimal/c1-exo1_main.cpp
+++ chapitre-01/exo1-le_programme_minimal/c1-exo1_main.cpp
@@ -5,7 +5,7 @@ int main()
   std::string nom , ville ;
 std::cin>>nom ;
 std::cin>>ville ;
-std::cout<<nom<<std::endl;
-std::cout<<ville<<std::endl;
+std::cout<<nom<<"\n";
+std::cout<<ville<<"\n";
 return 0 ;
 }
