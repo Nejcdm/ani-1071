0a1b234208d5fa3bd164079065d6bd11dd052976
Nejcdm <ninzizaemmanuel22@gmail.com>
2026-09-09 07:12:41 +0100

    Create minimal C++ program for input and output
    Add a minimal C++ program to read and print name and city.

---
+++ chapitre-01/exo01-le _programme_minimal/c1-exo1_main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+int main()
+{
+  std::string nom , ville ;
+std::cin>>nom ;
+std::cin>>ville ;
+std::cout<<nom<<std::endl;
+std::cout<<ville<<std::endl;
+retun 0 ;
+}
