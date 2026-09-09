a3e4b2606fa770970ce86c94384812f5e4fa8933
Nejcdm <ninzizaemmanuel22@gmail.com>
2026-09-09 07:51:19 +0100

    Update c1-exo1_main.cpp

--- chapitre-01/exo1-le_programme_minimal/c1-exo1_main.cpp
+++ chapitre-01/exo1-le_programme_minimal/c1-exo1_main.cpp
@@ -3,8 +3,10 @@ int main()
 {
 char nom[30];
   char ville[30];
-std::cin>>nom ;
-std::cin>>ville ;
+std::cout<<"entrer votre nom" ;
+std::cin.getline(nom,30);
+  std::cout<<"entrer votre ville" ;
+std::cin.getline(ville,30);
 std::cout<<nom<<std::endl;
 std::cout<<ville<<std::endl;
 return 0 ;
